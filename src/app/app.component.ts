60fdb9f15312e753b455d66326b8e151d53a39ad
Delmotte Pauline <paulinedelmotte63@gmail.com>
2021-01-21 09:08:49 +0100

    modify HTML et TS avec commentaires

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-list';
+
+  public todoArray: Array<string> = [
+    'item 1',
+    'item 2',
+    'item 3',
+    'item 4',
+    'item 5',
+    'foobar',
+  ];
+
+  /**
+   * This method is used in app.component.html on a keyup event on the text input.
+   * It take a KeyboardEvent as argument and return void (mean its return nothing).
+   */
+  public addItem($event: KeyboardEvent): void {
+    // Check if keyboard key pressed is Enter.
+    if ($event.code === 'Enter') {
+      // Get the event source html element. In our case, its an input element.
+      const $input: HTMLInputElement|null = $event.target as HTMLInputElement;
+
+      if ($input !== null) {
+        console.log($input.value);
+      }
+
+      console.log('Enter pressed');
+    }
+  }
 }
